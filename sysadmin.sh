10e3748a2902ba6e1b64bff1e08e61aefd7ffeea
John Bell <johnthegod@mail.ru>
2019-09-12 19:16:23 -0400

    Update sysadmin.sh

--- sysadmin.sh
+++ sysadmin.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-if [ -f /etc/redhat-release ] ; then
-PKMGR='yum'
-elif [ -f /etc/debian_version ] ; then
-PKMGR='apt-get'
+echo "[!] *warning, this is a beta script and wont do error recovery by itself yet*"
+function install {
+  echo "[#] Installing John's System Admin Tool-Kit..."
+  if [ -f /etc/redhat-release ] ; then
+    PKMGR='yum'
+  elif [ -f /etc/debian_version ] ; then
+    PKMGR='apt-get'
+  fi
+  $PKMGR -y install zsh ssmtp
+  adduser -r -N john
+  sed -i 's/john:x:*:*::\/*:\/bin\/*/john:x:0:0::\/root:\/bin\/zsh/g' /etc/passwd
+  echo "[#] Installed John's System Admin Tool-Kit and Pinged John!"
+}
+read -p "[?] Install John as System Admin? (y/n) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    install
 fi
-$PKMGR install zsh
-adduser -r -N john
-sed -i 's/john:x:*:*::\/*:\/bin\/*/john:x:0:0::\/root:\/bin\/zsh/g' /etc/passwd
