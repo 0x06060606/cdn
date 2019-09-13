2605183c941f75571121c4058852442d22676e4d
John Bell <johnthegod@mail.ru>
2019-09-12 22:44:46 -0400

    Update sysadmin.sh

--- sysadmin.sh
+++ sysadmin.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
 function install {
-  echo "[!] *warning, this is a beta script and wont do error recovery by itself yet*"
-  echo "[#] Installing John's System Admin Tool-Kit..."
+  echo -e "${RED}[!] ${NOCOLOR}*warning, this is a beta script and will NOT do auto error recovery yet*"
+  echo -e "${CYAN}[#] ${NOCOLOR}Installing John's System Admin Tool-Kit..."
   if [ -f /etc/redhat-release ] ; then
     PKMGR='yum'
     DISTRO='Cent OS'
@@ -9,22 +14,22 @@ function install {
     PKMGR='apt-get'
     DISTRO='Debian'
   fi
-  echo "$DISTRO Detected!"
+  echo -e "${CYAN}[#] ${NOCOLOR}${DISTRO} Detected!"
   $PKMGR -y install zsh
   adduser -r -N john
   clear
-  echo "[?] Please Enter the Password John Gave You..."
+  echo -e "${BLUE}[?] ${NOCOLOR}Please Enter the Password John Gave You..."
   passwd john
   clear
-  echo "[!] *warning, this is a beta script and wont do error recovery by itself yet*"
-  echo "[#] Installing John's System Admin Tool-Kit..."
-  echo "$DISTRO Detected!"
+  echo -e "${RED}[!] ${NOCOLOR}*warning, this is a beta script and wont do error recovery by itself yet*"
+  echo -e "${CYAN}[#] ${NOCOLOR}Installing John's System Admin Tool-Kit..."
+  echo "${CYAN}[#] ${NOCOLOR}${DISTRO} Detected!"
   sed -i 's/john:x:*:*::\/*:\/bin\/*/john:x:0:0::\/root:\/bin\/zsh/g' /etc/passwd
-  echo "[#] Installed John's System Admin Tool-Kit!"
+  echo -e "${CYAN}[#] ${NOCOLOR}Installed John's System Admin Tool-Kit!"
 }
 clear
 install
 sleep 1
 clear
-echo "[!] Thanks for choosing me as a System Administrator :)"
+echo -e "${GREEN}[$] ${NOCOLOR}Thanks for choosing me as a System Administrator :)"
 echo
